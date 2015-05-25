8a7ec02b135307458b5260ff06d1406e6c994731
giuseppefutia <giuseppe.futia@gmail.com>
2015-05-25 17:25:12 +0200

    Update matchers and templates for RDF of Italia  public contracts

--- matcher.js
+++ matcher.js
@@ -24,8 +24,11 @@ var combineMatchers = [
     {matcherName: "eventOrganizer", templateId: "eventInfoBox", combineIds: ['schema_name', 'email', 'telephone'], order: 6000000},
     {matcherName: "eventInfoBox", templateId: "eventInfoBox", combineIds: ['rdfschema_label', 'schema_description', 'schema_category', 'comment',"linkEventOrganizer","startDate","endDate"], order: 7000000 },
     {matcherName: "eventPOI", templateId: "POIInfoBox", combineIds: ['rdfschema_label', 'comment', 'foaf_mbox', 'foaf_phone', 'foaf_homepage', 'schema_category'], order: 7000000},
-    //{matcherName: "POIInfoBox", templateId: "POIInfoBox", combineIds: ['linkPOIEvent'], order: 7000000},
-    {matcherName: "eventSkos", templateId: "carousel", combineIds: ['skos_related'], order: 100000}
+    {matcherName: "eventSkos", templateId: "carousel", combineIds: ['skos_related'], order: 100000},
+    //Matchers for http://public-contracts.nexacenter.org/
+    {matcherName: "contractInformation", templateId: "pagingBox", combineIds: ['rdfschema_label'], order: 100000},
+    {matcherName: "wonContractInformation", templateId: "wonPagingBox", combineIds: ['rdfschema_label'], order: 100000}
+
 ];
 
 var linkMatchers = [
@@ -36,7 +39,10 @@ var linkMatchers = [
     {matcherName: "linkEventOrganizer", predicate: "http://schema.org/organizer", templateVariable: "organizerLink", templateId: "eventInfoBox", order: 200000, linkIds: ['eventOrganizer']},
     {matcherName: "linkEventAddress", predicate: "http://schema.org/address", templateVariable: "addressLink", templateId: "eventInfoBox", order: 200000, linkIds: ['eventAddress']},
     {matcherName: "linkPOIEvent", predicate: "http://sandbox.fusepool.info:8181/contains", templateVariable: "POILink", templateId: "POIInfoBox", order: 1100000, linkIds: ['eventPOI']}, // Usable only for Fusepool Platform
-    {matcherName: "linkSkosRelated", predicate: "http://www.w3.org/2004/02/skos/core#related", templateVariable: "skosLink", templateId: "carousel", order: 100000, linkIds: ['eventSkos']}
+    {matcherName: "linkSkosRelated", predicate: "http://www.w3.org/2004/02/skos/core#related", templateVariable: "skosLink", templateId: "carousel", order: 100000, linkIds: ['eventSkos']},
+    //Matchers for http://public-contracts.nexacenter.org/
+    {matcherName: "linkVatIDCig", predicate: "http://public-contracts.nexacenter.org/id/contract", templateVariable: "contractLink", templateId: "pagingBox", order: 200000, linkIds: ['contractInformation']},
+    {matcherName: "linkVatIDWonCig", predicate: "http://public-contracts.nexacenter.org/id/wonContract", templateVariable: "wonContractLink", templateId: "wonPagingBox", order: 200000, linkIds: ['wonContractInformation']}
 ];
 
 var predicateMatchers = [
@@ -131,5 +137,7 @@ var predicateMatchers = [
     {matcherName: "foaf_mbox", predicate: "http://xmlns.com/foaf/0.1/mbox", templateId: "text", templateVariable: "mbox", order: 1100000},
     {matcherName: "foaf_phone", predicate: "http://xmlns.com/foaf/0.1/phone", templateId: "text", templateVariable: "phone", order: 1100000},
     {matcherName: "foaf_homepage", predicate: "http://xmlns.com/foaf/0.1/homepage", templateId: "text", templateVariable: "homepage", order: 1100000},
-    {matcherName: "skos_related", predicate: "http://www.w3.org/2004/02/skos/core#related", templateId: "text", templateVariable: "text", order: 1100000}
+    {matcherName: "skos_related", predicate: "http://www.w3.org/2004/02/skos/core#related", templateId: "text", templateVariable: "text", order: 1100000},
+    //Matchers for http://public-contracts.nexacenter.org/
+    //{matcherName: "contracts", predicate: "http://public-contracts.nexacenter.org/id/contract", templateId: "pagingBox", templateVariable: "contracts", order: 1100000}
 ];
